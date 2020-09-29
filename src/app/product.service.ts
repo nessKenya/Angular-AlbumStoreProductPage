6226eb54e325f1c9d21e68e558c4a27bfe5915b6
nessKenya <nelson.muchonji@gmail.com>
2020-09-29 23:18:12 +0300

    Album Store Product Page with Angular final outcome project work done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,6 +5,8 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 
+import {Product} from './product';
+
 import {Album} from './album';
 
 @Injectable()
@@ -12,9 +14,15 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
+  private _productsUrl = '../assets/products.json';
+
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
 }
